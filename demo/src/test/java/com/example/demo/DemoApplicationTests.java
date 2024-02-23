e3e844ae7695d28f60a0c9f165137574f634753d
ilyaminsk <lobachilya@gmail.com>
2024-02-22 22:45:14 -0500

    FVF

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests1 {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
